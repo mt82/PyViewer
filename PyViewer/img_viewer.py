37acfbd97decc04af07c058a78c7dcfec68dc495
Matteo Tenti <tenti@bo.infn.it>
2021-09-22 23:59:12 +0200

    title as file name

--- PyViewer/img_viewer.py
+++ PyViewer/img_viewer.py
@@ -1,5 +1,8 @@
 from PyQt5 import QtCore
-from utils import getListOfFilesWithInfo
+try:
+    import PyViewer.utils as utl
+except:
+    import utils as utl
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QFileDialog
 from PyQt5.QtGui import QPixmap
@@ -9,40 +12,96 @@ folder="C:/Users/mt/OneDrive - Istituto Nazionale di Fisica Nucleare/Pictures/fr
 
 max_w, max_h = 1000, 1000
 
+def nextIndex(this_index, collection):
+    if len(collection) == 0:
+        this_index = -1
+    elif this_index == len(collection) - 1:
+        this_index = 0
+    else:
+        this_index += 1
+    return this_index
+
+def prevIndex(this_index, collection):
+    if len(collection) == 0:
+        this_index = -1
+    elif this_index == 0:
+        this_index = len(collection) - 1
+    else:
+        this_index -= 1
+    return this_index
+
 class App(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 image - pythonspot.com'
         self.left = 100
         self.top = 100
         self.width = max_w
         self.height = max_h
-        self.items = getListOfFilesWithInfo(folder)
+        self.items = utl.getListOfFilesWithInfo(folder)
+        self.initIndices()
         self.initUI()
     
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+    def initIndices(self):
+        self.img_index = 0 if len(self.items["image"]) > 0 else -1
+        self.vid_index = 0 if len(self.items["video"]) > 0 else -1
     
-        # Create widget
-        label = QLabel(self)
-        pixmap = QPixmap(self.items["image"][0]["path"])
+    def nextIndexImage(self):
+        self.img_index = nextIndex(self.img_index, self.items["image"])
+    
+    def nextIndexVideo(self):
+        self.vid_index = nextIndex(self.vid_index, self.items["video"])
+    
+    def prevIndexImage(self):
+        self.img_index = prevIndex(self.img_index, self.items["image"])
+    
+    def prevIndexVideo(self):
+        self.vid_index = prevIndex(self.vid_index, self.items["video"])
+    
+    def getImage(self):
+        if self.img_index == -1:
+            return None
+        else:
+            return self.items["image"][self.img_index]
+    
+    def getVideo(self):
+        if self.vid_index == -1:
+            return None
+        else:
+            return self.items["video"][self.vid_index]
+    
+    def loadImage(self, img):
+        self.setWindowTitle(img["name"])
+        pixmap = QPixmap(img["path"])
         scale_factor = max_w/pixmap.width() if max_w/pixmap.width() < max_h/pixmap.height() else  max_h/pixmap.height()
         width, height = int(pixmap.width()*scale_factor), int(pixmap.height()*scale_factor)
         pixmap = pixmap.scaled(width, height, QtCore.Qt.KeepAspectRatio)
-        label.setPixmap(pixmap)
+        self.label.setPixmap(pixmap)
         self.resize(width, height)
-        
-        self.show()
-		
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Space or event.key() == Qt.Key_Right:
-            self.test_method()
-
-    def test_method(self):
-        print('Space key pressed')
+        self.label.resize(width, height)
     
+    def initUI(self):
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.label = QLabel(self)
+        self.loadImage(self.getImage())
+        self.show()
+    
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.nextIndexImage()
+            self.loadImage(self.getImage())
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Space:
+            self.nextIndexImage()
+            self.loadImage(self.getImage())
+        elif event.key() == Qt.Key_Right:
+            self.nextIndexImage()
+            self.loadImage(self.getImage())
+        elif event.key() == Qt.Key_Left:
+            self.prevIndexImage()
+            self.loadImage(self.getImage())
+   
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
